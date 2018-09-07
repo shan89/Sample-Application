5013f9b0943f01c85e321fffd7f8ea5db6088211
Prem <premkumar.k@technoduce.com>
2018-09-07 15:21:26 +0530

    Minor changes.

--- SampleApplication/ViewController.swift
+++ SampleApplication/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
+        
         // Dispose of any resources that can be recreated.
     }
 
